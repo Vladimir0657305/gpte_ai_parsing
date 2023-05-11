7bf848be069c5591febfefc50c5142f27956f55f
Vladimir0657305 <vv0657305@gmail.com>
2023-05-11 11:44:13 +0300

    changes

--- index.py
+++ index.py
@@ -1,7 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-
 driver = webdriver.Chrome()
 driver.get("https://gpte.ai/")
 
@@ -14,12 +10,22 @@ while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     # Ждем, чтобы страница загрузилась
-    time.sleep(5)
+    time.sleep(10)
 
     # Проверяем, достигли ли мы конца страницы
     end_of_page = driver.execute_script("return window.innerHeight+window.pageYOffset >= document.body.offsetHeight;")
     if end_of_page:
-        break
+        # Если достигли конца страницы, ждем еще 5 секунд, чтобы дополнительный контент загрузился
+        time.sleep(10)
+
+        # Проверяем, есть ли на странице еще элементы для загрузки
+        end_of_page = driver.execute_script("return window.innerHeight+window.pageYOffset >= document.body.offsetHeight;")
+        if end_of_page:
+            # Если есть еще элементы для загрузки, прокручиваем страницу до конца снова
+            continue
+        else:
+            # Если больше нет элементов для загрузки, выходим из цикла
+            break
 
 # Теперь можно парсить содержимое страницы, включая все элементы, загруженные при скролле
 content = driver.page_source
