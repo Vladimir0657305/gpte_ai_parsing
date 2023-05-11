6ddaa17d7f22238e87a904cc19c2c8a773bdabc2
Vladimir0657305 <vv0657305@gmail.com>
2023-05-11 18:11:04 +0300

    changes

--- src/index.py
+++ src/index.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from urllib.parse import urljoin
+import requests
 import time
 import pandas as pd
 
+last_page = 3
 data_all = []
 
 def get_data(driver):
@@ -12,30 +15,59 @@ def get_data(driver):
     data = []
     
     # Находим все элементы статей на странице
-    # articles = driver.find_elements_by_css_selector('body div.viewport div.site-content div.total-wrap main.site-main div.inner div.post-feed article.post-card')
     articles = driver.find_elements(By.CSS_SELECTOR, 'body div.viewport div.site-content div.total-wrap main.site-main div.inner div.post-feed article.post-card')
-
 
     # Извлекаем информацию из каждой статьи и добавляем ее в список данных
     for article in articles:
-        title = article.find_element(By.CSS_SELECTOR, 'h2.post-card-title').text
-        category = article.find_element(By.CSS_SELECTOR, '.post-card-primary-tag').text
-        description = article.find_element(By.CSS_SELECTOR, 'div.post-card-excerpt').text
-        image_url = article.find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
+        title_elem = article.find_element(By.CSS_SELECTOR, 'h2.post-card-title')
+        title = title_elem.text if title_elem else None
+        
+        # category = None
+        for category_selector in ['.post-card-primary-tag', '.post-card-tags']:
+            try:
+                category_elem = article.find_element(By.CSS_SELECTOR, category_selector)
+                category = category_elem.text.split()[1] if category_elem else None
+                break
+            except NoSuchElementException:
+                category = 'Unknown'
+                continue
+        
+        description_elem = article.find_element(By.CSS_SELECTOR, 'div.post-card-excerpt')
+        description = description_elem.text if description_elem else None
+        
+        image_elem = article.find_element(By.CSS_SELECTOR, 'img')
+        image_url = image_elem.get_attribute('src') if image_elem else None
 
+        # Извлекаем ссылку
+        link = None
+        try:
+            link_elem = article.find_element(By.CSS_SELECTOR, 'a.post-card-content-link')
+            link = link_elem.get_attribute('href')
+        except NoSuchElementException:
+            link = 'Unknown'
+            pass
+        
+        # Переходим по ссылке и получаем URL после переадресации
+        response = requests.get(link)
+        final_url = response.url
+        
         print("Processing article:", title)
         print("Category:", category)
         print("Description:", description)
         print("Image URL:", image_url)
+        print("Link:", final_url)
 
         data.append({
             'Title': title,
             'Category': category,
             'Description': description,
-            'Image URL': image_url
+            'Image URL': image_url,
+            'Link': final_url
         })
 
     return data
+
+
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
@@ -46,7 +78,7 @@ data_all = get_data(driver)
 
 # Прокручиваем страницу до конца, чтобы загрузить все элементы
 num_page = 2
-while num_page <= 72:
+while num_page <= last_page:
     # Строим URL страницы для парсинга
     url = f"https://gpte.ai/page/{num_page}"
 
@@ -63,6 +95,6 @@ driver.quit()
 df = pd.DataFrame(data_all)
 
 # Сохраняем DataFrame в CSV-файл
-df.to_csv('data.csv', index=False)
+df.to_csv('D:/data.csv', index=False)
 
 
