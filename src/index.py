c197fd911260a7872ddee01fcff45542a1758630
Vladimir0657305 <vv0657305@gmail.com>
2023-05-11 11:31:14 +0300

    changes

--- src/index.py
+++ src/index.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 driver.get("https://gpte.ai/")
 
 # Ждем, чтобы страница загрузилась
-time.sleep(5)
+time.sleep(2)
 
 # Прокручиваем страницу до конца, чтобы загрузить все элементы
 while True:
@@ -14,7 +14,7 @@ while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     # Ждем, чтобы страница загрузилась
-    time.sleep(5)
+    time.sleep(2)
 
     # Проверяем, достигли ли мы конца страницы
     end_of_page = driver.execute_script("return window.innerHeight+window.pageYOffset >= document.body.offsetHeight;")
